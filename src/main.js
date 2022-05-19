e80a5871cbcab97da7b4f070e9a2723924714e64
kleklai <mmagallen@klaiklai.tech>
2022-05-19 22:42:33 +0800

    Change about description, added seo

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import CoolLightBox from 'vue-cool-lightbox';
 import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
 import './assets/scss/style.scss'
+import Meta from 'vue-meta'
 
 Vue.use(CoolLightBox);
 Vue.config.productionTip = false
+Vue.use(Meta)
 
 new Vue({
   router,
